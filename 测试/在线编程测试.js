b02c8632612050eca2e37cae192ded1452ef55a7
SuilandCoder <songjie0613@126.com>
2020-08-16 14:02:47 +0800

    LRU 缓存机制

---
+++ 测试/在线编程测试.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+
+};
---
+++ 测试/携程笔试训练.js
@@ -0,0 +1,6 @@
+let orderNum = 1001, inTime = 20180103, outTime = 20180105;
+let nums = [];
+let a = 20180602;
+if(a>=inTime){
+    nums.push(orderNum);
+}
